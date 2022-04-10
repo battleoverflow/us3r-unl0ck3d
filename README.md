0f5b2f9cfa9dd30041f4936a0ae01a1fb0041b7f
Hifumi1337 <56496067+Hifumi1337@users.noreply.github.com>
2022-04-09 22:39:32 -0500

    Added Windows (PowerShell) support!
    - Windows (PowerShell) support is a bit weird and may not be perfect using the octal architecture, but instead searches for the user's username
    - Added recursive option

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Us3r Unl0ck3d
-Us3r Unl0ck3d is a permissions scanner built to scan any Linux environment and return all accessible files by the currently logged-in user.
+Us3r Unl0ck3d is a permissions scanner built to scan most Linux and Windows environments, and return all accessible files by the currently logged-in user.
 
 ## Usage
 ```bash
@@ -8,26 +8,40 @@ $ python3 src/main.py
 
 ### Options
 ```bash
--h, --help      |   Displays help menu
--o, --octal     |   Type your octal as an integer to search for specific permissions (Ex: 777)
--f, --file      |   Store the output in a file (Format: log_<OCTAL>.txt)
--v, --verbose   |   If outputting to a file, this flag will need to be triggered to also print to the console
--c, --color     |   Add some color! (Off by default)
+-h, --help          |   Displays help menu
+-o, --octal         |   Type your octal as an integer to search for specific permissions (Ex: 777)
+-f, --file          |   Store the output in a file (Format: log_<OCTAL>.txt)
+-v, --verbose       |   If outputting to a file, this flag will need to be triggered to also print to the console
+-c, --color         |   Add some color! (Off by default) | Linux Only
+-r, --recursive     |   Recursively search the entire specified directory (May take a long time) | Windows Only
 ```
 
 ## Example(s)
+
+### Linux
 Displays all options with the 555 octal signature and pushes the data out to a file while printing the results to the terminal
 ```bash
-$ python3 src/main.py -o 555 -v -f
+$ python3 src/main.py -o 555 -vf
 ```
 
-Prints data to a file, but does not print anything to the terminal. Uses the default octal, 777.
+Prints data to a file, but does not print anything to the terminal. Uses the default octal, 777
 ```bash
 $ python3 src/main.py -f
 ```
 
+### Windows
+Searches the entire home directory of the logged-in user recursively
+```powershell
+> py src/main.py -r
+```
+
+Searches the root system (C:\), outputting all data to a file and displaying the output to the terminal recursively
+```powershell
+> py src/main.py -rfv
+```
+
 ## Helpful Octal(s)
-Here I've compiled a list of common octals you need to search for on a system.
+Here I've compiled a list of common octal(s) you may need to search for on a system:
 
 733 - Finds every option available to the owner + all writable and executable options for the public and group sections
 
--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
 from subprocess import getoutput
-import argparse
+import argparse, platform
 
 ###############################################
 #                                             #
@@ -13,10 +13,26 @@ parser = argparse.ArgumentParser()
 parser.add_argument('-o', '--octal', type=int, default=777, required=False, help="Type your octal as an integer (Ex: 777)")
 parser.add_argument('-f', '--file', default=False, required=False, help="Store the output in a file (Format: log_<OCTAL>.txt)", action="store_true")
 parser.add_argument('-v', '--verbose', default=False, required=False, help="If outputting to a file, this flag will need to be triggered to also print to the console", action="store_true")
-parser.add_argument('-c', '--color', default=False, required=False, help="Add some color! (True by default)", action="store_true")
+parser.add_argument('-c', '--color', default=False, required=False, help="Add some color! (True by default) | Linux Only", action="store_true")
+parser.add_argument('-r', '--recursive', default=False, required=False, help="Recursively search the entire specified directory (May take a long time) | Windows Only", action="store_true")
 args = parser.parse_args()
 
+version = "0.2.11"
+
 class Us3rUnl0ck3d:
+
+    def author(self):
+        print(f"""
+        #################################################################
+        #                                                               #
+        #   Us3r Unl0ck3d                                               #
+        #   Author: Hifumi1337                                          #
+        #   GitHub: https://github.com/Hifumi1337                       #
+        #   Repository: https://github.com/Hifumi1337/us3r-unl0ck3d     #
+        #                                                               #
+        #################################################################
+        v{version}
+        """)
 
     def main(self):
 
@@ -35,13 +51,14 @@ class Us3rUnl0ck3d:
             return int_to_str_conversion
         
         def color_scheme(r, g, b, text):
-            if args.color:
+            if args.color and platform.system() != 'Windows':
                 return "\033[38;2;{};{};{}m{} \033[38;2;255;255;255m".format(r, g, b, text)
 
         whoami = getoutput("whoami")
+        whoami_win = getoutput("powershell -Command  (Get-WMIObject -ClassName Win32_ComputerSystem).Username")
         permissions = convert_permissions(args.octal)
 
-        if args.color:
+        if args.color and platform.system() != 'Windows':
             print("[ {0}]  {1}   Entire Root System (Parameters: /*)"
                 .format(color_scheme(0, 255, 0, "0"), color_scheme(0, 255, 0, "=>")))
             
@@ -50,6 +67,15 @@ class Us3rUnl0ck3d:
             
             print("[ {0}]  {1}   Custom Directory (Ex: /etc)"
                 .format(color_scheme(0, 255, 0, "2"), color_scheme(0, 255, 0, "=>")))
+        elif platform.system() == 'Windows':
+            print("[{0}]  {1}   Entire Root System (Parameters: C:\*)"
+                .format("0", "=>"))
+            
+            print("[{0}]  {1}   Home Directory (Parameters: ~)"
+                .format("1", "=>"))
+            
+            print("[{0}]  {1}   Custom Directory (Ex: \Program Files)"
+                .format("2", "=>"))
         else:
             print("[{0}]  {1}   Entire Root System (Parameters: /*)"
                 .format("0", "=>"))
@@ -63,20 +89,51 @@ class Us3rUnl0ck3d:
         option = input("Choose Option: ")
 
         if option == '0':
-            access_granted = getoutput(f"ls -la /* | grep '{whoami}' | grep '{permissions}'")
-            print(f"\nSearching system for username: {whoami}")
-            print(f"Permissions Set: {permissions}")
+            if platform.system() == 'Windows':
+                if args.recursive:
+                    print("This might take awhile...")
+                    access_granted = getoutput(f"powershell -Command Get-ChildItem -Recurse C:\* | powershell -Command Get-Acl C:\* | powershell -Command findstr '{whoami_win}'")
+                else:
+                    access_granted = getoutput(f"powershell -Command Get-ChildItem C:\* | powershell -Command Get-Acl C:\* | powershell -Command findstr '{whoami_win}'")
+                
+                print(f"\nSearching system for username: {whoami_win}")
+            else:
+                access_granted = getoutput(f"ls -la /* | grep '{whoami}' | grep '{permissions}'")
+                
+                print(f"\nSearching system for username: {whoami}")
+                print(f"Permissions Set: {permissions}")
         elif option == '1':
-            access_granted = getoutput(f"ls -la ~ | grep '{whoami}' | grep '{permissions}'")
-            print(f"\nSearching system for username: {whoami}")
-            print(f"Permissions Set: {permissions}")
+            if platform.system() == 'Windows':
+                if args.recursive:
+                    print("This might take awhile...")
+                    access_granted = getoutput(f"powershell -Command Get-ChildItem -Recurse ~ | powershell -Command Get-Acl ~ | powershell -Command findstr '{whoami_win}'")
+                else:
+                    access_granted = getoutput(f"powershell -Command Get-ChildItem ~ | powershell -Command Get-Acl ~ | powershell -Command findstr '{whoami_win}'")
+                
+                print(f"\nSearching system for username: {whoami_win}")
+            else:
+                access_granted = getoutput(f"ls -la ~ | grep '{whoami}' | grep '{permissions}'")
+           
+                print(f"\nSearching system for username: {whoami}")
+                print(f"Permissions Set: {permissions}")
         elif option == '2':
             dir_name = input("Directory to search: ")
-            access_granted = getoutput(f"ls -la {dir_name} | grep '{whoami}' | grep '{permissions}'")
-            print(f"\nSearching system for username: {whoami}")
-            print(f"Permissions Set: {permissions}")
+
+            if platform.system() == 'Windows':
+                if args.recursive:
+                    print("This might take awhile...")
+                    access_granted = getoutput(f"powershell -Command Get-ChildItem -Recurse '{dir_name}' | powershell -Command Get-Acl '{dir_name}' | powershell -Command findstr '{whoami_win}'")
+                else:
+                    access_granted = getoutput(f"powershell -Command Get-ChildItem '{dir_name}' | powershell -Command Get-Acl '{dir_name}' | powershell -Command findstr '{whoami_win}'")
+                
+                print(f"\nSearching system for username: {whoami_win}")
+            else:
+                access_granted = getoutput(f"ls -la '{dir_name}' | grep '{whoami}' | grep '{permissions}'")
+            
+                print(f"\nSearching system for username: {whoami}")
+                print(f"Permissions Set: {permissions}")
         else:
-            if args.color:
+            if args.color and platform.system() != 'Windows':
                 print("\n{0}\n".format(color_scheme(255, 0, 0, "Unable to search or access system")))
             else:
                 print("\n{0}\n".format("Unable to search or access system"))
@@ -84,30 +141,49 @@ class Us3rUnl0ck3d:
         try:
             if access_granted != None:
 
-                if args.color:
+                if args.color and platform.system() != 'Windows':
                     print("\n=== {0} ===\n".format(color_scheme(0, 204, 0, "SEARCH INFO")))
                 else:
                     print("\n=== {0} ===\n".format("SEARCH INFO"))
-
-                print(f"The following files are accessible based your set parameters: {permissions}\n")
+                
+                if platform.system() == 'Windows':
+                    print(f"The following files are accessible based the set parameters: {whoami_win}\n")
+                else:
+                    print(f"The following files are accessible based the set parameters: {permissions}\n")
 
                 if args.file and args.verbose:
-                    print(access_granted)
+                    if platform.system() == 'Windows':
+                        print("Path     Owner       Access")
+                        print("-" * 50)
+                        print(access_granted)
+                    else:
+                        print(access_granted)
                 elif args.file and not args.verbose:
                     print("\nAll data is saved in the generated file")
                 else:
-                    print(access_granted)
+                    if platform.system() == 'Windows':
+                        print("Path     Owner               Access")
+                        print("-" * 50)
+                        print(access_granted)
+                    else:
+                        print(access_granted)
 
                 if args.file:
-                    with open(f'log_{args.octal}.txt', 'w') as f:
-                        f.write(access_granted)
+                    if platform.system() == 'Windows':
+                        with open(f'log_{whoami_win}.txt', 'w') as f:
+                            f.write("Path     Owner       Access")
+                            f.write("-" * 50)
+                            f.write(access_granted)
+                    else:
+                        with open(f'log_{args.octal}.txt', 'w') as f:
+                            f.write(access_granted)
             else:
-                if args.color:
+                if args.color and platform.system() != 'Windows':
                     print("\n{0}\n".format(color_scheme(255, 0, 0, "No data was generated")))
                 else:
                     print("No data was generated")
         except UnboundLocalError as e:
-            if args.color:
+            if args.color and platform.system() != 'Windows':
                 print("\n{0} {1}\n".format(color_scheme(255, 0, 0, "[ERROR] =>"), e))
             else:
                 print("[ERROR] => {0}".format(e))
@@ -115,4 +191,5 @@ class Us3rUnl0ck3d:
 
 if __name__ == '__main__':
     U3 = Us3rUnl0ck3d()
+    U3.author()
     U3.main()
